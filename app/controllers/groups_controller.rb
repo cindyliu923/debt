6f501171665673ef8a8d7a395e54e5c319ec9f1e
Cindy Liu <cindyliu923@gmail.com>
2018-08-07 00:36:41 +0800

    user can create groups in index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,21 @@
 class GroupsController < ApplicationController
   def index
+    @groups = current_user.groups.all
+    @group = current_user.groups.build
   end
+
+  def create
+    if @group = current_user.groups.create(group_params)
+      redirect_to group_path(@group)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,3 @@
 class Group < ApplicationRecord
-   has_and_belongs_to_many :users
+  has_and_belongs_to_many :users
 end
